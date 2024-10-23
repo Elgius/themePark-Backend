d1e4c77de4ff3329c57c6386fd339eaeafd629ab
Elgius <114131653+Elgius@users.noreply.github.com>
2024-10-23 19:09:42 +0500

    this is OP

--- main.py
+++ main.py
@@ -16,6 +16,13 @@ def main_root():
 # command to run code:
 # fastapi dev main.py
 
+# shut down
+# ctrl + C
+
+
 # note: code will run on localHost:8000
 # main.py is the entry point of the application. refer to the docs, google or GPT and get the party started!
 # env variables coming soon
+
+# note: using a virtual environment for this is totally fine
+# another note: ignore the docker file
